b224ed428f755f1cd4cfb3f48a8a23eb8c96308b
Ivan Budanov <32747977+iabudanov@users.noreply.github.com>
2019-02-23 13:31:52 +0000

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,3 @@
-'''value
-if (list[value] >= list[value-1]) and (list[value] <= list[value+1])
-'''
 import bisect
 
 def insertion_sort(int_list):
@@ -12,14 +9,3 @@ def insertion_sort(int_list):
     print(empty_list)
 
 insertion_sort([1, 3, 2, 45, 5])
-
-
-
-'''loop through the list
-append first value into empty_list
-if the value in front is smaller than the value before it, do bisect() to insert the value in the
-right place in empty_list1
-
-
-[1, 4, 8, 56, 100]
-[1, 2, 3, 4 , 5]'''
